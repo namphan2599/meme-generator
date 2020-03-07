1d1fbcb65464ae60144bbbd35842679e0e6caa31
Naem <anomynous992@gmail.com>
2020-03-07 21:16:22 +0700

    Updates

---
+++ precache-manifest.95da8c897e92dc39ff33dde9348459ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c7d61bbc4faac7d70faed3de1a2892",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "3552520054fac0dc483b",
+    "url": "/meme-generator/static/css/main.c3b5c54f.chunk.css"
+  },
+  {
+    "revision": "dd905a3291acc7a0bc62",
+    "url": "/meme-generator/static/js/2.b23607f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/meme-generator/static/js/2.b23607f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3552520054fac0dc483b",
+    "url": "/meme-generator/static/js/main.83046da0.chunk.js"
+  },
+  {
+    "revision": "cc48f9e75a7fed5be940",
+    "url": "/meme-generator/static/js/runtime-main.8af17244.js"
+  }
+]);
